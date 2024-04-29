6b4caa8e3a1764fb3829e96c37bf2879e96c6dce
foglar <kohout.fi.2023@skola.ssps.cz>
2024-04-29 09:45:03 +0200

    Fix - Check for job completion

--- astrometry.py
+++ astrometry.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+TIME_OUT = 5
 
 class AstrometryClient:
     """Client for interacting with the Astrometry API.
@@ -54,31 +55,33 @@ class AstrometryClient:
             logging.critical("Authentication failed. Session key not obtained.")
 
     def upload_image(self, image_path):
-            """Uploads an image to the Astrometry.net API and returns the submission ID.
+        """Uploads an image to the Astrometry.net API and returns the submission ID.
 
-            Args:
-                image_path (str): The path to the image file to be uploaded.
+        Args:
+            image_path (str): The path to the image file to be uploaded.
 
-            Returns:
-                str: The submission ID of the uploaded image, or None if the upload failed.
-            """
-            if not self.session:
-                logging.warning("Please authenticate first.")
-                return None
+        Returns:
+            str: The submission ID of the uploaded image, or None if the upload failed.
+        """
+        if not self.session:
+            logging.warning("Please authenticate first.")
+            return None
 
-            files = {
-                "request-json": (None, json.dumps({"session": self.session})),
-                "file": (image_path, open(image_path, "rb"), "application/octet-stream"),
-            }
-            response = requests.post("http://nova.astrometry.net/api/upload", files=files)
+        files = {
+            "request-json": (None, json.dumps({"session": self.session})),
+            "file": (image_path, open(image_path, "rb"), "application/octet-stream"),
+        }
+        response = requests.post("http://nova.astrometry.net/api/upload", files=files)
 
-            logging.info("Status code: %s", response.json())
-            if response.status_code == 200:
-                logging.info("Image upload successful. Submission ID: %s", response.json()["subid"])
-                return response.json()["subid"]
-            else:
-                logging.error("Image upload failed. Status code: %s", response.status_code)
-                return None
+        logging.info("Status code: %s", response.json())
+        if response.status_code == 200:
+            logging.info(
+                "Image upload successful. Submission ID: %s", response.json()["subid"]
+            )
+            return response.json()["subid"]
+        else:
+            logging.error("Image upload failed. Status code: %s", response.status_code)
+            return None
 
     def check_job_status(self, job_id):
         """Check the status of a job.
@@ -105,7 +108,7 @@ class AstrometryClient:
                 f"Error checking job status. Status code: {response.status_code}"
             )
             return None
-        
+
     def check_submission_status(self, submission_id):
         """Check the status of a submission.
 
@@ -131,6 +134,25 @@ class AstrometryClient:
                 f"Error checking submission status. Status code: {response.status_code}"
             )
             return None
+        
+    def is_job_done(self, job_id):
+        """Check if a job is done.
+
+        Args:
+            job_id (int): The ID of the job to check.
+
+        Returns:
+            bool: True if the job is done, False otherwise.
+
+        """
+        url = f"http://nova.astrometry.net/api/submissions/{job_id}"
+        response = requests.get(url)
+        if response.json().get("job_calibrations") != []:
+            logging.info("Job is done.")
+            return True
+        else:
+            logging.info("Job is not done.")
+            return False
 
     def get_calibration(self, job_id):
         """Get calibration information for a job.
@@ -159,7 +181,7 @@ class AstrometryClient:
                 f"Error getting calibration information. Status code: {response.status_code}"
             )
             return None
-        
+
     def get_wcs_file(self, job_id, save_path):
         """
         Get the URL of the WCS (World Coordinate System) file for a given job ID
@@ -184,7 +206,9 @@ class AstrometryClient:
             logging.info("WCS file downloaded successfully.")
             return response.url
         else:
-            logging.error(f"Error getting WCS file. Status code: {response.status_code}")
+            logging.error(
+                f"Error getting WCS file. Status code: {response.status_code}"
+            )
             return None
 
 
@@ -210,10 +234,16 @@ def main():
         logging.warning("Job is not successful. Aborting...")
         return
     
-    while client.check_submission_status(submission_id) == [] or client.check_submission_status(submission_id) == [None]:
-        sleep(ConfigLoader().get_value_from_data("timeout"))
+    while client.is_job_done(submission_id) == False:
+        sleep(TIME_OUT)
 
-    wcs_file_url = client.get_wcs_file(submission_id , "./test.wcs")
+    # Check submission status
+    # while client.check_submission_status(
+    #     submission_id
+    # ) == [] or client.check_submission_status(submission_id) == [None]:
+    #     sleep(ConfigLoader().get_value_from_data("timeout"))
+
+    wcs_file_url = client.get_wcs_file(submission_id, "./test.wcs")
     if not wcs_file_url:
         logging.error("Failed to get WCS file URL.")
         return
